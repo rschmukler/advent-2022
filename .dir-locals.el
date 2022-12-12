ab652828dc465541dfa4ced87bc09427391aa51e
Ryan Schmukler <rschmukler@gmail.com>
2022-12-12 11:39:37 -0600

    chore: add dir-locals
    Add a dir-locals file with some useful vars

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((clojure-mode . ((cider-clojure-cli-aliases . ":dev")
+                  (eval . (aggressive-indent-mode)))))
